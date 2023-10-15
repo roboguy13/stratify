ae48573f19226212fb15f6ef32bba299e944d85b
David Young <roboguy06@gmail.com>
2023-10-15 11:22:55 -0500

    A very barebones REPL interface

--- index.js
+++ index.js
@@ -1,12 +1,475 @@
-// output/Effect.Console/foreign.js
-var log = function(s) {
-  return function() {
-    console.log(s);
+(() => {
+  // output/Data.Function/index.js
+  var flip = function(f) {
+    return function(b) {
+      return function(a) {
+        return f(a)(b);
+      };
+    };
   };
-};
 
-// output/Main/index.js
-var main = /* @__PURE__ */ log("\u{1F35D}");
-export {
-  main
-};
+  // output/Data.Unit/foreign.js
+  var unit = void 0;
+
+  // output/Data.Functor/index.js
+  var map = function(dict) {
+    return dict.map;
+  };
+
+  // output/Control.Apply/index.js
+  var apply = function(dict) {
+    return dict.apply;
+  };
+
+  // output/Control.Applicative/index.js
+  var pure = function(dict) {
+    return dict.pure;
+  };
+  var liftA1 = function(dictApplicative) {
+    var apply2 = apply(dictApplicative.Apply0());
+    var pure1 = pure(dictApplicative);
+    return function(f) {
+      return function(a) {
+        return apply2(pure1(f))(a);
+      };
+    };
+  };
+
+  // output/Control.Bind/index.js
+  var bind = function(dict) {
+    return dict.bind;
+  };
+  var bindFlipped = function(dictBind) {
+    return flip(bind(dictBind));
+  };
+
+  // output/Data.Bounded/foreign.js
+  var topChar = String.fromCharCode(65535);
+  var bottomChar = String.fromCharCode(0);
+  var topNumber = Number.POSITIVE_INFINITY;
+  var bottomNumber = Number.NEGATIVE_INFINITY;
+
+  // output/Data.Maybe/index.js
+  var Nothing = /* @__PURE__ */ function() {
+    function Nothing2() {
+    }
+    ;
+    Nothing2.value = new Nothing2();
+    return Nothing2;
+  }();
+  var Just = /* @__PURE__ */ function() {
+    function Just2(value0) {
+      this.value0 = value0;
+    }
+    ;
+    Just2.create = function(value0) {
+      return new Just2(value0);
+    };
+    return Just2;
+  }();
+  var functorMaybe = {
+    map: function(v) {
+      return function(v1) {
+        if (v1 instanceof Just) {
+          return new Just(v(v1.value0));
+        }
+        ;
+        return Nothing.value;
+      };
+    }
+  };
+  var map2 = /* @__PURE__ */ map(functorMaybe);
+  var applyMaybe = {
+    apply: function(v) {
+      return function(v1) {
+        if (v instanceof Just) {
+          return map2(v.value0)(v1);
+        }
+        ;
+        if (v instanceof Nothing) {
+          return Nothing.value;
+        }
+        ;
+        throw new Error("Failed pattern match at Data.Maybe (line 67, column 1 - line 69, column 30): " + [v.constructor.name, v1.constructor.name]);
+      };
+    },
+    Functor0: function() {
+      return functorMaybe;
+    }
+  };
+  var bindMaybe = {
+    bind: function(v) {
+      return function(v1) {
+        if (v instanceof Just) {
+          return v1(v.value0);
+        }
+        ;
+        if (v instanceof Nothing) {
+          return Nothing.value;
+        }
+        ;
+        throw new Error("Failed pattern match at Data.Maybe (line 125, column 1 - line 127, column 28): " + [v.constructor.name, v1.constructor.name]);
+      };
+    },
+    Apply0: function() {
+      return applyMaybe;
+    }
+  };
+
+  // output/Effect/foreign.js
+  var pureE = function(a) {
+    return function() {
+      return a;
+    };
+  };
+  var bindE = function(a) {
+    return function(f) {
+      return function() {
+        return f(a())();
+      };
+    };
+  };
+
+  // output/Control.Monad/index.js
+  var ap = function(dictMonad) {
+    var bind2 = bind(dictMonad.Bind1());
+    var pure3 = pure(dictMonad.Applicative0());
+    return function(f) {
+      return function(a) {
+        return bind2(f)(function(f$prime) {
+          return bind2(a)(function(a$prime) {
+            return pure3(f$prime(a$prime));
+          });
+        });
+      };
+    };
+  };
+
+  // output/Effect/index.js
+  var $runtime_lazy = function(name15, moduleName, init) {
+    var state2 = 0;
+    var val;
+    return function(lineNumber) {
+      if (state2 === 2)
+        return val;
+      if (state2 === 1)
+        throw new ReferenceError(name15 + " was needed before it finished initializing (module " + moduleName + ", line " + lineNumber + ")", moduleName, lineNumber);
+      state2 = 1;
+      val = init();
+      state2 = 2;
+      return val;
+    };
+  };
+  var monadEffect = {
+    Applicative0: function() {
+      return applicativeEffect;
+    },
+    Bind1: function() {
+      return bindEffect;
+    }
+  };
+  var bindEffect = {
+    bind: bindE,
+    Apply0: function() {
+      return $lazy_applyEffect(0);
+    }
+  };
+  var applicativeEffect = {
+    pure: pureE,
+    Apply0: function() {
+      return $lazy_applyEffect(0);
+    }
+  };
+  var $lazy_functorEffect = /* @__PURE__ */ $runtime_lazy("functorEffect", "Effect", function() {
+    return {
+      map: liftA1(applicativeEffect)
+    };
+  });
+  var $lazy_applyEffect = /* @__PURE__ */ $runtime_lazy("applyEffect", "Effect", function() {
+    return {
+      apply: ap(monadEffect),
+      Functor0: function() {
+        return $lazy_functorEffect(0);
+      }
+    };
+  });
+  var functorEffect = /* @__PURE__ */ $lazy_functorEffect(20);
+
+  // output/Effect.Exception/foreign.js
+  function error(msg) {
+    return new Error(msg);
+  }
+  function throwException(e) {
+    return function() {
+      throw e;
+    };
+  }
+
+  // output/Effect.Exception/index.js
+  var $$throw = function($4) {
+    return throwException(error($4));
+  };
+
+  // output/Effect.Unsafe/foreign.js
+  var unsafePerformEffect = function(f) {
+    return f();
+  };
+
+  // output/Stratify.Utils/index.js
+  var error2 = function($2) {
+    return unsafePerformEffect($$throw($2));
+  };
+
+  // output/Web.DOM.NonElementParentNode/foreign.js
+  function _getElementById(id) {
+    return function(node) {
+      return function() {
+        return node.getElementById(id);
+      };
+    };
+  }
+
+  // output/Data.Nullable/foreign.js
+  function nullable(a, r, f) {
+    return a == null ? r : f(a);
+  }
+
+  // output/Data.Nullable/index.js
+  var toMaybe = function(n) {
+    return nullable(n, Nothing.value, Just.create);
+  };
+
+  // output/Web.DOM.NonElementParentNode/index.js
+  var map3 = /* @__PURE__ */ map(functorEffect);
+  var getElementById = function(eid) {
+    var $2 = map3(toMaybe);
+    var $3 = _getElementById(eid);
+    return function($4) {
+      return $2($3($4));
+    };
+  };
+
+  // output/Web.Event.EventTarget/foreign.js
+  function eventListener(fn) {
+    return function() {
+      return function(event) {
+        return fn(event)();
+      };
+    };
+  }
+  function addEventListener(type) {
+    return function(listener) {
+      return function(useCapture) {
+        return function(target5) {
+          return function() {
+            return target5.addEventListener(type, listener, useCapture);
+          };
+        };
+      };
+    };
+  }
+
+  // output/Web.HTML/foreign.js
+  var windowImpl = function() {
+    return window;
+  };
+
+  // output/Unsafe.Coerce/foreign.js
+  var unsafeCoerce2 = function(x) {
+    return x;
+  };
+
+  // output/Web.Internal.FFI/foreign.js
+  function _unsafeReadProtoTagged(nothing, just, name15, value12) {
+    if (typeof window !== "undefined") {
+      var ty = window[name15];
+      if (ty != null && value12 instanceof ty) {
+        return just(value12);
+      }
+    }
+    var obj = value12;
+    while (obj != null) {
+      var proto = Object.getPrototypeOf(obj);
+      var constructorName = proto.constructor.name;
+      if (constructorName === name15) {
+        return just(value12);
+      } else if (constructorName === "Object") {
+        return nothing;
+      }
+      obj = proto;
+    }
+    return nothing;
+  }
+
+  // output/Web.Internal.FFI/index.js
+  var unsafeReadProtoTagged = function(name15) {
+    return function(value12) {
+      return _unsafeReadProtoTagged(Nothing.value, Just.create, name15, value12);
+    };
+  };
+
+  // output/Web.HTML.HTMLButtonElement/index.js
+  var toEventTarget = unsafeCoerce2;
+  var fromElement = /* @__PURE__ */ unsafeReadProtoTagged("HTMLButtonElement");
+
+  // output/Web.HTML.HTMLDocument/index.js
+  var toNonElementParentNode = unsafeCoerce2;
+
+  // output/Data.Traversable/foreign.js
+  var traverseArrayImpl = function() {
+    function array1(a) {
+      return [a];
+    }
+    function array2(a) {
+      return function(b) {
+        return [a, b];
+      };
+    }
+    function array3(a) {
+      return function(b) {
+        return function(c) {
+          return [a, b, c];
+        };
+      };
+    }
+    function concat2(xs) {
+      return function(ys) {
+        return xs.concat(ys);
+      };
+    }
+    return function(apply2) {
+      return function(map5) {
+        return function(pure3) {
+          return function(f) {
+            return function(array) {
+              function go2(bot, top2) {
+                switch (top2 - bot) {
+                  case 0:
+                    return pure3([]);
+                  case 1:
+                    return map5(array1)(f(array[bot]));
+                  case 2:
+                    return apply2(map5(array2)(f(array[bot])))(f(array[bot + 1]));
+                  case 3:
+                    return apply2(apply2(map5(array3)(f(array[bot])))(f(array[bot + 1])))(f(array[bot + 2]));
+                  default:
+                    var pivot = bot + Math.floor((top2 - bot) / 4) * 2;
+                    return apply2(map5(concat2)(go2(bot, pivot)))(go2(pivot, top2));
+                }
+              }
+              return go2(0, array.length);
+            };
+          };
+        };
+      };
+    };
+  }();
+
+  // output/Web.HTML.HTMLTextAreaElement/foreign.js
+  function value11(textarea) {
+    return function() {
+      return textarea.value;
+    };
+  }
+  function setValue11(value12) {
+    return function(textarea) {
+      return function() {
+        textarea.value = value12;
+      };
+    };
+  }
+
+  // output/Web.HTML.HTMLTextAreaElement/index.js
+  var fromElement2 = /* @__PURE__ */ unsafeReadProtoTagged("HTMLTextAreaElement");
+
+  // output/Web.HTML.Window/foreign.js
+  function document(window2) {
+    return function() {
+      return window2.document;
+    };
+  }
+
+  // output/Web.HTML.Event.EventTypes/index.js
+  var click2 = "click";
+
+  // output/Web.UIEvent.MouseEvent/index.js
+  var fromEvent = /* @__PURE__ */ unsafeReadProtoTagged("MouseEvent");
+
+  // output/Main/index.js
+  var pure2 = /* @__PURE__ */ pure(applicativeEffect);
+  var map4 = /* @__PURE__ */ map(functorEffect);
+  var bindFlipped2 = /* @__PURE__ */ bindFlipped(bindMaybe);
+  var onClick = function(inputArea) {
+    return function(outputArea) {
+      return function(event) {
+        var v = fromEvent(event);
+        if (v instanceof Nothing) {
+          return pure2(unit);
+        }
+        ;
+        if (v instanceof Just) {
+          return function __do2() {
+            var inputVal = value11(inputArea)();
+            if (inputVal === ":quit") {
+              return setValue11("Goodbye!")(outputArea)();
+            }
+            ;
+            return setValue11("You typed: " + inputVal)(outputArea)();
+          };
+        }
+        ;
+        throw new Error("Failed pattern match at Main (line 40, column 3 - line 46, column 82): " + [v.constructor.name]);
+      };
+    };
+  };
+  var fromJust2 = function(v) {
+    return function(v1) {
+      if (v1 instanceof Just) {
+        return v1.value0;
+      }
+      ;
+      if (v1 instanceof Nothing) {
+        return error2("fromJust: " + v);
+      }
+      ;
+      throw new Error("Failed pattern match at Main (line 48, column 1 - line 48, column 45): " + [v.constructor.name, v1.constructor.name]);
+    };
+  };
+  var main = function __do() {
+    var win = windowImpl();
+    var doc = document(win)();
+    var rootNode = toNonElementParentNode(doc);
+    var runBtn = map4(function(v) {
+      return bindFlipped2(fromElement)(v);
+    })(getElementById("runButton")(rootNode))();
+    var inputArea = map4(function() {
+      var $20 = fromJust2("inputArea");
+      return function($21) {
+        return $20(function(v) {
+          return bindFlipped2(fromElement2)(v);
+        }($21));
+      };
+    }())(getElementById("inputArea")(rootNode))();
+    var outputArea = map4(function() {
+      var $22 = fromJust2("outputArea");
+      return function($23) {
+        return $22(function(v) {
+          return bindFlipped2(fromElement2)(v);
+        }($23));
+      };
+    }())(getElementById("outputArea")(rootNode))();
+    if (runBtn instanceof Nothing) {
+      return error2("runButton")();
+    }
+    ;
+    if (runBtn instanceof Just) {
+      var listener = eventListener(onClick(inputArea)(outputArea))();
+      return addEventListener(click2)(listener)(false)(toEventTarget(runBtn.value0))();
+    }
+    ;
+    throw new Error("Failed pattern match at Main (line 32, column 3 - line 36, column 71): " + [runBtn.constructor.name]);
+  };
+
+  // <stdin>
+  main();
+})();
--- src/Main.purs
+++ src/Main.purs
@@ -2,9 +2,58 @@ module Main where
 
 import Prelude
 
+import Stratify.Utils
+
 import Effect (Effect)
-import Effect.Console (log)
+import Web.HTML (window)
+import Web.HTML.HTMLDocument (toNonElementParentNode)
+import Web.HTML.Window (document)
+import Web.DOM.NonElementParentNode (getElementById)
+import Web.HTML.HTMLTextAreaElement as TextAreaElement
+import Web.HTML.HTMLTextAreaElement (HTMLTextAreaElement)
+import Web.DOM
+-- import Web.DOM.HTMLInputElement (value, setValue)
+import Web.Event.EventTarget (addEventListener, eventListener)
+import Web.UIEvent.MouseEvent (MouseEvent, fromEvent)
+import Web.Event.Event
+import Web.HTML.HTMLButtonElement as Button
+import Web.HTML.Event.EventTypes (click)
+
+import Stratify.Syntax.Core.Term
+import Stratify.Eval.NbE
+import Stratify.Syntax.Name
+import Stratify.Ppr
+
+import Data.Maybe (Maybe(..))
 
 main :: Effect Unit
 main = do
-  log "🍝"
+  win <- window
+  doc <- document win
+  let rootNode = toNonElementParentNode doc
+  runBtn <- (Button.fromElement =<< _) <$> getElementById "runButton" rootNode
+  inputArea <- (fromJust "inputArea" <<< (TextAreaElement.fromElement =<< _)) <$> getElementById "inputArea" rootNode
+  outputArea <- (fromJust "outputArea" <<< (TextAreaElement.fromElement =<< _)) <$> getElementById "outputArea" rootNode
+  case runBtn of
+    Nothing -> error "runButton"
+    Just btn -> do
+      listener <- eventListener (onClick inputArea outputArea)
+      addEventListener click listener false (Button.toEventTarget btn)
+
+onClick :: HTMLTextAreaElement -> HTMLTextAreaElement -> Event -> Effect Unit
+onClick inputArea outputArea event =
+  case fromEvent event of
+    Nothing -> pure unit
+    Just _ -> do
+      inputVal <- TextAreaElement.value inputArea
+      -- TextAreaElement.setValue (ppr' (eval))
+      case inputVal of
+        ":quit" -> TextAreaElement.setValue "Goodbye!" outputArea
+        _      -> TextAreaElement.setValue ("You typed: " <> inputVal) outputArea
+
+fromJust :: forall a. String -> Maybe a -> a
+fromJust _ (Just x) = x
+fromJust msg Nothing = error $ "fromJust: " <> msg
+
+-- ppr' (Right x) = ppr x
+-- ppr' (Left y) = y
